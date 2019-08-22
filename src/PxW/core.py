0c71d7587384f2ee7fa48e11ce837f533bb1a917
eliavw <elia.vw@gmail.com>
2019-08-22 17:37:23 +0200

    Getting close to an operational fit function

---
+++ src/PxW/core.py
@@ -0,0 +1,19 @@
+
+WEKA = ""
+
+
+class Weka(object):
+    # Main Methods
+    def __init__(self):
+
+        self.model_filename = None
+
+        return
+
+    def fit(self, **kwargs):
+        return
+
+    def predict(self, **kwargs):
+        return
+
+    # Tier 2 Methods
